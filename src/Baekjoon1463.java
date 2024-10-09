4c8795835c154a815c4044fc8a6cb99f7de5e8f8
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-10 01:36:55 +0900

    1463 X

--- src/Baekjoon1463.java
+++ src/Baekjoon1463.java
@@ -3,11 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Baekjoon1463 {
+    static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int X = Integer.parseInt(br.readLine());
-        int[] dp = new int[X + 1];
+        // 메모이제이션
+        dp = new int[X + 1];
+        dp[0] = dp[1] = dp[2] = dp[3] = 1;
+        System.out.println(ref(X));
+    }
 
-
+    public static int ref(int x) {
+        if (dp[x] == 0) {
+            if (x % 6 == 0) {
+                dp[x] = Math.min(ref(x - 1), Math.min(ref(x / 2), ref(x/3))) + 1;
+            } else if (x % 3 == 0) {
+                dp[x] = Math.min(ref(x / 3), ref(x - 1))+1;
+            } else if (x % 2 == 0) {
+                dp[x] = Math.min(ref(x / 2), ref(x - 1))+1;
+            } else {
+                dp[x] = ref(x - 1)+1;
+            }
+        }
+        return dp[x];
     }
 }
