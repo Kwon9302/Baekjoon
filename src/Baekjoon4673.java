0a20dc81a15ef646f884d7b2e17d2e7d160802a0
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-12 01:29:55 +0900

    241011 4673

--- src/Baekjoon4673.java
+++ src/Baekjoon4673.java
@@ -1,12 +1,10 @@
-public class Baekjoon {
+public class Baekjoon4673 {
     public static void main(String[] args) {
         int[] dp = new int[10001];
         dp[0] = dp[1] = 0;
 
         for (int i = 1; i < 10001; i++) {
-            if (dp[i] != 0) {
-                continue;
-            }
+//            System.out.println("for문 돌아~~");
             int ii=i;
             String I = Integer.toString(i);
             for (int j = 0; j < I.length(); j++) {
@@ -15,8 +13,12 @@ public class Baekjoon {
             if(ii>10000){
                 continue;
             }
+//            System.out.println("i : "+i);
+//            System.out.println("ii : " + ii);
             dp[ii]  = i;
         }
+
+
         for (int i = 1; i < 10001; i++) {
             if(dp[i] == 0) {
                 System.out.println(i);
