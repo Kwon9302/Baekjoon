ec7f3c7f22a58eee9ba1de57cd0f663a7377f06b
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-13 19:06:11 +0900

    2164~ing

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        // 2164번
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
