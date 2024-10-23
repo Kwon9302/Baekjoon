1d57bb085b126a3bda7262f0e4e0f1fd5f3869ae
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-23 19:56:50 +0900

    1932 start

---
+++ src/Baekjoon1932.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class Baekjoon1932 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            while (st.hasMoreTokens()) {
+
+            }
+        }
+    }
+}
