d8c023382b6879ddfda1f041939b56e26422fe10
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-23 20:45:02 +0900

    1932 O

--- src/Baekjoon1932.java
+++ src/Baekjoon1932.java
@@ -10,13 +10,32 @@ public class Baekjoon1932 {
 
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N];
-
-        for (int i = 0; i < N; i++) {
+        int[] temp = new int[N];
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        dp[0] = Integer.parseInt(st1.nextToken());
+        int result =dp[0];
+        for (int i = 1; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
-            while (st.hasMoreTokens()) {
-
+            int count = st.countTokens();
+            for (int j = 0; j < count; j++) {
+                int x = Integer.parseInt(st.nextToken());
+                if (j == 0) {
+                    temp[j] = dp[0] + x;
+                } else {
+                    temp[j] = Math.max(dp[j-1]+x, dp[j]+x);
+                }
+            }
+            for (int j = 0; j < N; j++) {
+                if(temp[j]==0) {
+                    continue;
+                } else {
+                    dp[j] = temp[j];
+                }
             }
         }
+        for (int i = 0; i < N; i++) {
+            result = Math.max(result, dp[i]);
+        }
+        System.out.println(result);
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Test {
-    public static void main(String[] args) {
-        Deque<Integer> stack = new ArrayDeque<>();
-
-        for (int i = 0; i < 10; i++) {
-            stack.push(i);
-        }
-        System.out.println(stack.pollLast());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(st.nextToken());
+//        }
+        System.out.println(st.countTokens());
     }
 }
