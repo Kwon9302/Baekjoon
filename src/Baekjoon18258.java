dfad94a987d278cd3e366e8fc4a09cede66e6143
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-27 02:20:43 +0900

    18258 O

--- src/Baekjoon18258.java
+++ src/Baekjoon18258.java
@@ -1,11 +1,51 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Baekjoon18258 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Deque<Integer> queue = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String x = st.nextToken();
+            if (x.equals("push")) {
+                queue.addLast(Integer.parseInt(st.nextToken()));
+            } else if (x.equals("pop")) {
+                if (queue.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(queue.removeFirst() + "\n");
+                }
+            } else if (x.equals("size")) {
+                bw.write(queue.size() + "\n");
+            } else if (x.equals("empty")) {
+                bw.write((queue.isEmpty() ? "1" : "0") + "\n");
+            } else if (x.equals("front")) {
+                if (queue.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(queue.peekFirst() + "\n");
+                }
+            } else if (x.equals("back")) {
+                if (queue.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(queue.peekLast() + "\n");
+                }
+            }
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
