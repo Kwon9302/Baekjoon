374da0a533739147156788f6921dc2737f057439
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-31 03:18:24 +0900

    11279 X, Heap 익히기

--- src/Baekjoon11279.java
+++ src/Baekjoon11279.java
@@ -1,9 +1,27 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Baekjoon11279 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
+
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(br.readLine());
+            // 비어있는 상태에서 0이 나올때
+            if(a==0 && pq.isEmpty()){
+                System.out.println(0);
+                continue;
+            }
+            if(a==0){
+                System.out.println(pq.poll());
+            }
+            pq.add(a);
+
+        }
     }
 }
