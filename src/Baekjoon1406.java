250bb6de6325aa95855534026d764da8280188de
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-10-31 20:13:46 +0900

    1406 start

---
+++ src/Baekjoon1406.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon1406 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] st = new int[100000];
+
+        int N = Integer.parseInt(br.readLine());
+
+    }
+}
