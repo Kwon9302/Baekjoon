2cf35cdef9998b481072c4bb39ebb0c601cde158
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-11-02 14:27:28 +0900

    2504 start

--- src/Baekjoon4949.java
+++ src/Baekjoon4949.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class Baekjoon4949 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String x;
         while (true) {
             Stack<String> stack = new Stack<>();
