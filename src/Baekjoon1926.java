22876799c2a0b4f28bf62a99c706caf54f72dfca
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-11-03 20:47:33 +0900

    bfs기초 문제 자동완성 끄고 해보기

--- src/Baekjoon1926.java
+++ src/Baekjoon1926.java
@@ -1,10 +1,72 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Baekjoon1926 {
-    public static void main(String[] args) {
+    static int n;
+    static int m;
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int maxPictureSize=0;
+    static int pictureCount=0;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
+        arr = new int[n][m];
+        visited = new boolean[n][m];
 
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        // 배열확인
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(arr[i][j]==1 && !visited[i][j]){
+                    bfs(i,j);
+                    pictureCount++;
+                }
+            }
+        }
+        System.out.println(pictureCount);
+        System.out.println(maxPictureSize);
+    }
+
+    public static void bfs(int i, int j) {
+        int size =1;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i, j});
+        visited[i][j] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int k = 0; k < 4; k++) {
+                int x = cur[0] + dx[k];
+                int y = cur[1] + dy[k];
+
+                if(x>=0 && y>=0 && x<n && y<m && !visited[x][y] && arr[x][y]==1) {
+                    visited[x][y] = true;
+                    q.add(new int[]{x, y});
+                    size++;
+                }
+            }
+        }
+        maxPictureSize = Math.max(size,maxPictureSize);
     }
 }
