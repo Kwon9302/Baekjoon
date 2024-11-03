dc5cc7075a70c67720eb4df19d61c6137674f03d
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-11-03 20:28:46 +0900

    1926 start

---
+++ src/Baekjoon1926.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Baekjoon1926 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
--- src/Baekjoon2504.java
+++ src/Baekjoon2504.java
@@ -1,9 +1,32 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Baekjoon2504 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int sum=0;
+        for (int i = 0; i < str.length(); i++) {
+            char x = str.charAt(i);
+            if(x=='(' || x=='['){
+                stack.push(x);
+            } else if (x==')') {
+                char y = stack.pop();
+                if(x!=y){
+                    System.out.println(0);
+                    return;
+                }
+            } else if (x==']') {
+                char y = stack.pop();
+                if(x!=y){
+                    System.out.println(0);
+                    return;
+                }
+            }
+        }
 
     }
 }
