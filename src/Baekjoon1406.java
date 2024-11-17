21c39f83fc26596c55ee576c88a9eb37704157da
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-11-17 13:40:04 +0900

    1406 커서문제 ListIterator 활용

--- src/Baekjoon1406.java
+++ src/Baekjoon1406.java
@@ -13,22 +13,22 @@ public class Baekjoon1406 {
         for (int i = 0; i < a.length(); i++) {
             arr.add(a.charAt(i)+"");
         }
-        ListIterator<String> cursorIndex = arr.listIterator(arr.size());
+        ListIterator<String> cursorIndex = arr.listIterator(arr.size()); // 커서의 시작 위치르 맨 뒤로 초기화
 
         for (int i = 0; i < m; i++) {
             StringTokenizer x = new StringTokenizer(br.readLine());
             String ch = x.nextToken();
             if(ch.equals("L")){
                 if(cursorIndex.hasPrevious()){
-                    cursorIndex.previous();
+                    cursorIndex.previous(); // 커서 위치 왼쪽으로 이
                 }
             } else if(ch.equals("D")){
                 if(cursorIndex.hasNext()){
-                    cursorIndex.next();
+                    cursorIndex.next(); // 커서를 우측으로 이동
                 }
             } else if(ch.equals("P")){
                 String xx = x.nextToken();
-                cursorIndex.add(xx);
+                cursorIndex.add(xx); // add는 커서 왼쪽에 글자를 추가함
             } else if(ch.equals("B")){
                 if(cursorIndex.hasPrevious()){
                     cursorIndex.previous();
